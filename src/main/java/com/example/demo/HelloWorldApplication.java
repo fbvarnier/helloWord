d4d6869b9df8d8e0311765d7017408df6267fbfa
Fabiano Varnier <36083655+fbvarnier@users.noreply.github.com>
2021-11-03 21:27:02 -0300

    Update HelloWorldApplication.java

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class HelloWorldApplication {
@@ -18,6 +19,7 @@ public class HelloWorldApplication {
 class HelloWorldController {
 
 	@GetMapping
+	@CrossOrigin(origins = "*", maxAge = 3600)
 	public String hello(){
 		return "Hello";
 	}
